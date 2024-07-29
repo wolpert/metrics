2aaa646f84c18bf0bd03eafb2d36999b2b591b9b
Ned Wolpert <ned.wolpert@gmail.com>
2024-07-29 08:44:38 -0700

    Continued work on declaritive metrics annotation

--- metrics-declarative/src/main/java/com/codeheadsystems/metrics/declarative/DeclarativeMetricsManager.java
+++ metrics-declarative/src/main/java/com/codeheadsystems/metrics/declarative/DeclarativeMetricsManager.java
@@ -3,6 +3,7 @@ package com.codeheadsystems.metrics.declarative;
 import com.codeheadsystems.metrics.MetricFactory;
 import com.codeheadsystems.metrics.Metrics;
 import com.codeheadsystems.metrics.impl.NullMetricsImpl;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -17,6 +18,7 @@ public class DeclarativeMetricsManager {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(DeclarativeMetricsManager.class);
   private static final NullMetricsImpl NULL_METRICS = new NullMetricsImpl();
+  private static final AtomicBoolean INITIALIZED = new AtomicBoolean(false);
   private static volatile Metrics METRICS;
 
   /**
@@ -43,14 +45,21 @@ public class DeclarativeMetricsManager {
    * @throws Throwable the throwable
    */
   @Around("execution(* *(..)) && @annotation(com.codeheadsystems.metrics.declarative.DeclarativeFactory)")
-  public Object aroundDeclarativeFactory(final ProceedingJoinPoint point) throws Throwable {
-    System.out.println("aroundDeclarativeFactory({})" + point);
-    final Object pjp = point.proceed(point.getArgs());
-    if (pjp instanceof MetricFactory) {
-      LOGGER.info("aroundDeclarativeFactory() - setting metrics to {}", pjp);
-      METRICS = (Metrics) pjp;
+  public MetricFactory aroundDeclarativeFactory(final ProceedingJoinPoint point) throws Throwable {
+    LOGGER.debug("aroundDeclarativeFactory({})", point);
+    Object result = point.proceed(point.getArgs());
+    if (!(result instanceof final MetricFactory factory)) {
+      final String msg = String.format("aroundDeclarativeFactory() - Unable to cast to MetricFactory, got %s from %s",
+          result, point.getSignature().getName());
+      LOGGER.error(msg);
+      throw new ClassCastException(msg);
     }
-    return pjp;
+    LOGGER.info("aroundDeclarativeFactory() - setting metrics to {}", factory);
+    METRICS = factory;
+    if (!INITIALIZED.compareAndSet(false, true)) {
+      LOGGER.warn("aroundDeclarativeFactory() - Initialized MetricsFactory already called");
+    }
+    return factory;
   }
 
   /**
@@ -62,7 +71,25 @@ public class DeclarativeMetricsManager {
    */
   @Around("@annotation(com.codeheadsystems.metrics.declarative.Metrics)")
   public Object aroundMetrics(final ProceedingJoinPoint point) throws Throwable {
-    System.out.println("aroundMetrics({})" + point);
-    return point.proceed(point.getArgs());
+    final String metricName = getMetricName(point);
+    final boolean initialized = INITIALIZED.get();
+    LOGGER.trace("aroundMetrics({}, {})", metricName, initialized);
+    if (!initialized) {
+      return point.proceed(point.getArgs());
+    } else {
+      return METRICS.time(metricName, () -> {
+        try {
+          return point.proceed(point.getArgs());
+        } catch (final Throwable t) {
+          throw new RuntimeException(t); // TODO: This is bad but time is using a generic exception and java is chocking.
+        }
+      });
+    }
+  }
+
+  private String getMetricName(final ProceedingJoinPoint point) {
+    final String className = point.getSignature().getDeclaringType().getSimpleName();
+    final String method = point.getSignature().getName();
+    return String.format("%s.%s", className, method);
   }
 }
