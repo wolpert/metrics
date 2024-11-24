4a4e6168bdfd4e8209c605a825add96d637fada2
Ned Wolpert <ned.wolpert@gmail.com>
2024-11-24 12:06:49 -0700

    version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,8 @@ nexusPublishing {
 }
 allprojects {
     group = "com.codeheadsystems"
-    version = "2.0.9-SNAPSHOT"
+    version = "2.0.9"
+//    version = "2.0.9-SNAPSHOT"
 
     repositories {
         mavenLocal()
